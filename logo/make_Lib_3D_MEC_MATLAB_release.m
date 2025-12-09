5f4af3073a46c0a0c1780c0c8096c1d02dd9cede
ferhalo24 <ferhalo24@gmail.com>
2025-12-09 13:25:45 +0100

    Add files via upload

---
+++ logo/make_Lib_3D_MEC_MATLAB_release.m
@@ -0,0 +1,6 @@
+
+release=date;
+% file=[mfilename('fullpath'),'/release.mat']
+file=[pwd,'/release.mat']
+
+save(file,'release' )
---
+++ logo/my_callback_fcn.m
@@ -0,0 +1,3 @@
+function my_callback_fcn(obj, event, text_arg)
+
+delete(text_arg);
